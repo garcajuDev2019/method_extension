e5c2845d4bde2ffb218e7a650a0fad0930f080bd
garcajuDev2019 <garcajuvueling@gmail.com>
2019-02-10 12:56:46 +0100

    Agregar archivos de proyecto.

---
+++ method_extension/Prime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_extension
+{
+    static class Prime
+    {
+        public static bool IsPrime(this int num)
+        {
+            int counter = 2;
+            bool prime = true;
+
+            while(prime && (counter != num))
+            {
+                if (num % counter == 0) { prime = false; }
+                counter++;
+            }
+            return prime;
+        }
+    }
+}
---
+++ method_extension/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace method_extension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            Console.Write("Please, insert a number: ");
+            try
+            {
+                num = int.Parse(Console.ReadLine());
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Wrong or invalid numeric value!");
+            }
+
+            bool isPrime = num.IsPrime(); //Calling to extended method
+            PrintResult(num, isPrime);
+        }
+
+        static void PrintResult(int num, bool isPrime)
+        {
+            StringBuilder result;
+
+            if (isPrime)
+            {
+                result = new StringBuilder(" is a prime number");
+                result.Insert(0, num);
+            }
+            else
+            {
+                result = new StringBuilder(" is not a prime number");
+                result.Insert(0, num);
+            }
+            Console.WriteLine(value: result);
+        }
+    }
+}
